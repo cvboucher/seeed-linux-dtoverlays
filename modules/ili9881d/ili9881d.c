7438ece52b0a8887266f8268a9beef947c5abc7d
Craig Boucher <craig@wesvic.com>
2024-06-18 10:32:41 -0700

    Update ili9881d.c - Remove retry but continue commands in prepare.

--- modules/ili9881d/ili9881d.c
+++ modules/ili9881d/ili9881d.c
@@ -371,23 +371,21 @@ static int ili9881d_prepare(struct drm_panel *panel)
 	for (i = 0; i < ctx->desc->init_length; i++) {
 		const struct ili9881d_instr *instr = &ctx->desc->init[i];
 
-		do {
-			if (instr->op == ILI9881C_SWITCH_PAGE)
-				ret = ili9881d_switch_page(ctx, instr->arg.page);
-			else if (instr->op == ILI9881C_COMMAND)
-				ret = ili9881d_send_cmd_data(ctx, instr->arg.cmd.cmd,
-						      	instr->arg.cmd.data);
+		if (instr->op == ILI9881C_SWITCH_PAGE) 
+			ret = ili9881d_switch_page(ctx, instr->arg.page);
+		else if (instr->op == ILI9881C_COMMAND)
+			ret = ili9881d_send_cmd_data(ctx, instr->arg.cmd.cmd,
+						instr->arg.cmd.data);
 
-			if (ret) {
-				printk(KERN_INFO "ili9881d_prepare: ForLoop-Step: %d, ret: %d\n", i, ret);
-				/*
-				dsi_status = 1;
-				return ret;
-				*/
-				msleep(20);
-			}
+		if (ret) {
+			printk(KERN_INFO "ili9881d_prepare: ForLoop-Step: %d, ret: %d\n", i, ret);
+			/*
+			dsi_status = 1;
+			return ret;
+			*/
+			msleep(10);
+		}
 		
-		} while (ret);
 	}
 
 	ret = ili9881d_switch_page(ctx, 0);
